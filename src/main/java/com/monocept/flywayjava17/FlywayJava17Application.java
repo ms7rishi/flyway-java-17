2bb0af72a90b31437bd9f8d517fd8288bfec4b43
RishiSingh <rishisingh@monocept.com>
2022-09-09 15:52:54 +0530

    initial setup with flyway

---
+++ src/main/java/com/monocept/flywayjava17/FlywayJava17Application.java
@@ -0,0 +1,13 @@
+package com.monocept.flywayjava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayJava17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/monocept/flywayjava17/FlywayJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monocept.flywayjava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
